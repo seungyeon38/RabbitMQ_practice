890fe0f60eda333427c1f6bbd2d483f207b98503
seungyeon38 <seongyeon38@gmail.com>
2024-02-29 14:19:29 +0900

    message acknowledgment (ack)

--- re_worker.exs
+++ re_worker.exs
@@ -5,7 +5,7 @@
 defmodule Worker do
   def wait_for_messages(channel) do
     receive do
-      {:basic_deliver, payload, _meta} ->
+      {:basic_deliver, payload, meta} ->
         IO.puts " [x] Received #{payload}"
 
         payload
@@ -15,13 +15,20 @@ defmodule Worker do
         |> :timer.sleep
 
         IO.puts " [x] Done."
+        AMQP.Basic.ack(channel, meta.delivery_tag) # 중요
 
         wait_for_messages(channel)
     end
   end
 end
 
-AMQP.Basic.consume(channel, "task_queue", nil, no_ack: true)
+# AMQP.Basic.consume(channel, "task_queue", nil, no_ack: true)
+AMQP.Basic.consume(channel, "task_queue", nil)
 
 IO.puts " [*] Waiting for messages. To exit press CTRL+C, CTRL+C"
 Worker.wait_for_messages(channel)
+
+# worker가 종료되면, processing되고 있거나 아직 handle되지 않은 메세지는 없어진다.
+# 어떠한 task도 종료되지 않게 하기 위해서는, 해당 worker가 죽었을 때, 그 task가 다른 worker에게 전송되어야 한다.
+# message acknowledgments (ack)
+# AMQP.Basic.ack
